611f2b486a9b81d48ac8321f776b4f0c00882e87
Gariyaan panda <gariyanpanda@gmail.com>
2026-06-08 20:28:16 +0530

    Update pair_with_difference.py

--- optimization_practice/pair_with_difference.py
+++ optimization_practice/pair_with_difference.py
@@ -79,3 +79,11 @@ overall O(n) algorithm.
 =================================================
 
 """
+def has_difference_brute_force(nums, k):
+    n = len(nums)
+    for i in range(n):
+        for j in range(i + 1, n):
+            if abs(nums[i] - nums[j]) == k:
+                return True
+    return False
+   
