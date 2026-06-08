b7351fbca493239c4f9374a2da4602b76703e203
Gariyaan panda <gariyanpanda@gmail.com>
2026-06-08 20:29:22 +0530

    Update two_sum.py

--- optimization_practice/two_sum.py
+++ optimization_practice/two_sum.py
@@ -87,3 +87,9 @@ O(1) time, giving an overall O(n) algorithm.
 =================================================
 
 """
+def two_sum_brute(nums, target):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+               
